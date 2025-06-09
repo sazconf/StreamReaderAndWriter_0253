95341ca6e256ebf6138ff5f6c5c3288def218831
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-06-09 17:50:47 +0700

    Complete the program

--- IOfilewithpath.cpp
+++ IOfilewithpath.cpp
@@ -32,3 +32,31 @@ int main (){
     outfile<<baris<<endl;
 }
 
+outfile.close();
+
+//membuat obyek input file stream dalam mode membaca
+ifstream infile;
+//membuka file
+infile.open("contohfile.txt");
+
+cout << endl
+<<">= Membuka dan membaca file"<<endl;
+// jika file add maka
+if(
+    infile.is_open())
+    {
+        //melakukan perulagan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        ///tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else {
+        cout << "Unable to open file";
+    }
+
+}
