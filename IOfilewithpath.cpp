87fdcfec8c9ac3affeb9f1f38307d7949ab0d41b
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-06-09 17:49:44 +0700

    Open file

--- IOfilewithpath.cpp
+++ IOfilewithpath.cpp
@@ -5,3 +5,17 @@
 
 using namespace std;
 
+int main (){
+    string baris;
+
+    //membuat obyek output file stream dalam mode menulis
+    ofstream outfile;
+
+    //membuka file
+    outfile.open("contohfile.txt");
+
+    cout<< ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+
+    
